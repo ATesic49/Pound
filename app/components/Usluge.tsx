625749720796f8ac42544df8ed6e58086e7f5de3
ATesic49 <atesic7@gmail.com>
2023-06-26 00:58:53 +0200

    s

--- app/components/Usluge.tsx
+++ app/components/Usluge.tsx
@@ -15,8 +15,84 @@ const montserrat = Montserrat({
 export default function Usluge() {
   return (
     <div id="usluge-poc" className={aboshione.className}>
-      <h1>Najuzbudljiviji provod u Srbiji</h1>
-      <div className="ispod"></div>
+      <div className="h1">
+        <h1>
+          Sve <span className={montserrat.className}>š</span>to ti treba na
+          jednom mestu
+        </h1>
+      </div>
+      <div className="ispod">
+        <h2>
+          Proslave 18 Ro<span className={montserrat.className}>đ</span>endana{" "}
+          <span className="span">!</span>
+        </h2>
+        <div className="p">
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae ab
+            debitis, perferendis fuga sapiente hic veritatis inventore cum animi
+            vero! Cupiditate magni sint ullam provident.
+          </p>
+        </div>
+        <div className="swiper">
+          <div className="holder">
+            <img src="/party.svg" alt="" />
+          </div>
+        </div>
+      </div>
+      <div className="ispod">
+        <h2>
+          Proslave 18 Ro<span className={montserrat.className}>đ</span>endana{" "}
+          <span className="span">!</span>
+        </h2>
+        <div className="p">
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae ab
+            debitis, perferendis fuga sapiente hic veritatis inventore cum animi
+            vero! Cupiditate magni sint ullam provident.
+          </p>
+        </div>
+        <div className="swiper">
+          <div className="holder">
+            <img src="/party.svg" alt="" />
+          </div>
+        </div>
+      </div>
+      <div className="ispod">
+        <h2>
+          Proslave 18 Ro<span className={montserrat.className}>đ</span>endana{" "}
+          <span className="span">!</span>
+        </h2>
+        <div className="p">
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae ab
+            debitis, perferendis fuga sapiente hic veritatis inventore cum animi
+            vero! Cupiditate magni sint ullam provident.
+          </p>
+        </div>
+        <div className="swiper">
+          <div className="holder">
+            <img src="/party.svg" alt="" />
+          </div>
+        </div>
+      </div>
+      <div className="ispod">
+        <h2>
+          Korporativna okupljanja
+          <span className="span">!</span>
+        </h2>
+        <div className="p">
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae ab
+            debitis, perferendis fuga sapiente hic veritatis inventore cum animi
+            vero! Cupiditate magni sint ullam provident.
+          </p>
+        </div>
+        <div className="swiper">
+          <div className="holder">
+            <img src="/korporativna.svg" alt="" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
